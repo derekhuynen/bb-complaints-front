8c6106bf18455441a8e14da5b328fa0aa7f4a5d4
Derek <derek.huynen@gmail.com>
2021-04-18 01:31:06 -0700

    fixed map

--- src/Map.js
+++ src/Map.js
@@ -7,13 +7,11 @@ import InfoWindowEx from './InfoWindowEx'
 export class Maps extends Component {
 
     state = {
-        data: [],
-        loc_x: 0,
-        loc_y: 0,
+        latitude: 0,
+        longitude: 0,
         locRendered: false,
         showingInfoWindow: false,
-        activeMarker: {},
-        selectedPlace: {}
+        activeMarker: {}
     }
 
     getUserLocation = () => {
@@ -24,20 +22,17 @@ export class Maps extends Component {
 
     showPosition = position => {
         this.setState({
-            loc_x: position.coords.latitude,
-            loc_y: position.coords.longitude,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
             locRendered: true,
         })
-        console.log(position.coords.latitude, position.coords.longitude)
     }
 
     centerMoved = (mapProps, map) => {
         this.setState({
-            loc_x: map.center.lat(),
-            loc_y: map.center.lng()
-
+            latitude: map.center.lat(),
+            longitude: map.center.lng()
         })
-        console.log(map.center.lat(),map.center.lng())
     }
 
     componentWillMount() {
@@ -50,38 +45,38 @@ export class Maps extends Component {
                 {this.state.locRendered ? (
                     <Map
                         google={this.props.google}
-                        zoom={14}
+                        zoom={15}
+                        minZoom={6}
+                        maxZoom={18}
                         styles={this.props.mapStyles}
                         disableDefaultUI={true}
                         onDragend={this.centerMoved}
                         initialCenter={{
-                            lat: this.state.loc_x,
-                            lng: this.state.loc_y
+                            lat: this.state.latitude,
+                            lng: this.state.longitude
                         }}
                     >
-
                         <Marker
                             position={{ lat: 34.2360064, lng: -116.90311679999999 }}
-                            title={"Hello"}
+                            address={"123 Walker Street"}
                             onClick={this.onMarkerClick}
+                            icon={{url: "/marker.svg",
+                                scaledSize: new window.google.maps.Size(12, 12)}}
                         />
-
                         <InfoWindowEx marker={this.state.activeMarker} visible={this.state.showingInfoWindow}>
                             <div>
-                                HELLO!
+                                <h3>Address: {this.state.activeMarker.address}</h3>
                             </div>
                         </InfoWindowEx>
                     </Map>
-                ) : null}
+                ) : <h1>Map Failed to Render</h1>}
             </div>
         )
     }
     onMarkerClick = (props, marker) => {
         this.setState({
-            selectedPlace: props.data,
             activeMarker: marker,
             showingInfoWindow: true,
-            redirect: false
         })
     }
 }
@@ -89,216 +84,11 @@ export class Maps extends Component {
 Maps.defaultProps = {
     mapStyles: [
         {
-            elementType: 'geometry',
-            stylers: [
-                {
-                    color: '#242f3e'
-                }
-            ]
+            featureType: "all",
+            elementType: "labels.icon",
+            stylers: [{ visibility: "off" }],
         },
-        {
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#746855'
-                }
-            ]
-        },
-        {
-            elementType: 'labels.text.stroke',
-            stylers: [
-                {
-                    color: '#242f3e'
-                }
-            ]
-        },
-        {
-            featureType: 'administrative.land_parcel',
-            elementType: 'labels',
-            stylers: [
-                {
-                    visibility: 'off'
-                }
-            ]
-        },
-        {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#d59563'
-                }
-            ]
-        },
-        {
-            featureType: 'poi',
-            elementType: 'labels.text',
-            stylers: [
-                {
-                    visibility: 'off'
-                }
-            ]
-        },
-        {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#d59563'
-                }
-            ]
-        },
-        {
-            featureType: 'poi.business',
-            stylers: [
-                {
-                    visibility: 'off'
-                }
-            ]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [
-                {
-                    color: '#263c3f'
-                }
-            ]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'labels.text',
-            stylers: [
-                {
-                    visibility: 'off'
-                }
-            ]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#6b9a76'
-                }
-            ]
-        },
-        {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [
-                {
-                    color: '#38414e'
-                }
-            ]
-        },
-        {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [
-                {
-                    color: '#212a37'
-                }
-            ]
-        },
-        {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#9ca5b3'
-                }
-            ]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [
-                {
-                    color: '#746855'
-                }
-            ]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [
-                {
-                    color: '#1f2835'
-                }
-            ]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#f3d19c'
-                }
-            ]
-        },
-        {
-            featureType: 'road.local',
-            elementType: 'labels',
-            stylers: [
-                {
-                    visibility: 'off'
-                }
-            ]
-        },
-        {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [
-                {
-                    color: '#2f3948'
-                }
-            ]
-        },
-        {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#d59563'
-                }
-            ]
-        },
-        {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [
-                {
-                    color: '#17263c'
-                }
-            ]
-        },
-        {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [
-                {
-                    color: '#515c6d'
-                }
-            ]
-        },
-        {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [
-                {
-                    color: '#17263c'
-                }
-            ]
-        }
     ]
-}
-
-function mapStateToProps(state) {
-    return {
-        x: state.x,
-        y: state.y
-    }
 }
 
 export default GoogleApiWrapper({
